0024f54c2e0cfa7a1348bea2e35fb1511dbd16ea
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-03-10 15:31:49 +0100

    More updates

--- src/plotscript.py
+++ src/plotscript.py
@@ -3,72 +3,147 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os.path
 
-# %matplotlib inline  # jupyter notebook
+"""
+Setup
+"""
 
-# create dictionary for the methods to map to numbers
-nums = [1, 2, 3, 4, 5, 6]
-methods = ["Yggdrasil", "CJDNS", "", "", "", ""]
+# create array for the methods to map to numbers
+
+methods = ["Yggdrasil", "CJDNS", "Husarnet",
+           "Port Forwarding", "Wireguard", "Proxy Server"]
 
 
-
-# Load data
 _ = os.path.dirname(__file__)
-data_p = pd.read_csv(_ + '/../trials/ping_test_danter-ygg.csv')
+
+x = list(range(6))  # 6 methods being tested
+
+plt.figure(figsize=(8.8, 5.5))
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{:.1f}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+
+                    ha='center', va='bottom')
+
+
+###################
+# plot iperf data
+###################
 data = pd.read_csv(_ + '/../trials/tryme.csv')
 data2 = pd.read_csv(_ + '/../trials/tryme2.csv')
 
-
-# Plot iperf data
-x = range(2)
-
 ax = plt.subplot(111)
-# plt.figure(figsize=(6.8, 4.2))
 
-#compute averages and plot those
-A=list(data['bits_per_second'])
-Ae=A[0::2]
-Ao=A[1::2]
-B=list(data2['bits_per_second'])
-Be=B[0::2]
-Bo=B[1::2]
+# compute averages and plot those
+A = list(data['bits_per_second'])
+Ae = A[0::2]
+Ao = A[1::2]
+B = list(data2['bits_per_second'])
+Be = B[0::2]
+Bo = B[1::2]
 av1 = sum(Ae)/(len(Ae)*1000000)
 av12 = sum(Ao)/(len(Ao)*1000000)
 av2 = sum(Be)/(len(Bo)*1000000)
 av22 = sum(Bo)/(len(Bo)*1000000)
 
-#create client and server average lists
-even = [av1, av2]
-odd = [av12, av22]
+# create client and server average lists
+even = [av1, av2, 0, 0, 0, 0]
+odd = [av12, av22, 0, 0, 0, 0]
 
-# print(x.shape)
-
-
-ax.bar(x, even, width=0.2, color='b', align='edge')
-ax.bar(x, odd, width=-0.2, color='g', align='edge')
-
-
+l1 = ax.bar(x, even, width=0.3, color='b', align='edge')
+l2 = ax.bar(x, odd, width=-0.3, color='g', align='edge')
+ax.legend((l1, l2), ("Client->Server", "Server->Client"))
+autolabel(l1)
+autolabel(l2)
 
 # plt.bar(x, data['bits_per_second'])
-# plt.xticks(x, data['connection_type'])
-plt.xlabel('Connection Type')
+ax.set_title('Transmission Speed by Connection Type')
+ax.set_xticks(x)
+ax.set_xticklabels(methods)
+
 plt.ylabel('Speed in Mbps')
-plt.savefig(_ + "/../charts/iperf_chart.png")
+plt.savefig(_ + "/../charts/iperf_chart.png", dpi=250)
+
+# clear legend for axes
+ax.get_legend().remove()
+plt.cla()
 
 
-
+###################
 # plot ping data
-C=list(data_p['roundtrip_time'])
-C_min=min(C)
-C_max=max(C)
-C_avg=sum(C)/len(C)
+###################
+data_p = pd.read_csv(_ + '/../trials/ping_test_danter-ygg.csv')
 
-ax.bar(1-0.2, C_min, width=0.2, color='b', align='center')
-ax.bar(1, C_avg, width=0.2, color='g', align='center')
-ax.bar(1+0.2, C_max, width=0.2, color='r', align='center')
+C = list(data_p['roundtrip_time'])
+min_c = min(C)
+max_c = max(C)
+avg_c = sum(C)/len(C)
+
+C_min = [min_c, 0, 0, 0, 0, 0]
+C_max = [max_c, 0, 0, 0, 0, 0]
+C_avg = [avg_c, 0, 0, 0, 0, 0]
+
+ax = plt.subplot(111)
+
+ax.set_xticks(x)
+ax.set_xticklabels(methods)
+
+l1 = ax.bar([i+0.3 for i in x], C_min, width=0.3, color='b', align='center')
+l2 = ax.bar(x, C_avg, width=0.3, color='g', align='center')
+l3 = ax.bar([i-0.3 for i in x], C_max, width=0.3, color='r', align='center')
+ax.legend((l1, l2, l3), ("Minimum roundtrip time",
+                         "Average roundtrip time", "Maximum roundtrip time"))
+ax.set_title('Packet Roundtrip Time by Connection Type')
+autolabel(l1)
+autolabel(l2)
+autolabel(l3)
 
 plt.xlabel('Connection Type')
 plt.ylabel('Time in ms')
 
-plt.savefig(_ + "/../charts/ping_chart.png")
+plt.savefig(_ + "/../charts/ping_chart.png", dpi=250)
 
-# plt.show()
+# clear legend for axes
+ax.get_legend().remove()
+plt.cla()
+
+###################
+# plot ssh speed data
+###################
+
+data_s = pd.read_csv(_ + '/../trials/scp-speed.csv')
+
+U = list(data_s['Upload_Speed'])
+D = list(data_s['Download_Speed'])
+
+avg_U = sum(U)/len(U)
+avg_D = sum(D)/len(D)
+
+U_all = [avg_U, 0, 0, 0, 0, 0]
+D_all = [avg_D, 0, 0, 0, 0, 0]
+
+ax = plt.subplot(111)
+
+ax.set_xticks(x)
+ax.set_xticklabels(methods)
+
+l1 = ax.bar(x, U_all, width=0.3, color='b', align='edge')
+l2 = ax.bar(x, D_all, width=-0.3, color='g', align='edge')
+
+ax.legend((l1, l2), ("Average Upload Speed",
+                     "Average Download Speed"))
+
+autolabel(l1)
+autolabel(l2)
+
+
+plt.xlabel('Connection Type')
+plt.ylabel('Speed in Mbps')
+
+plt.savefig(_ + "/../charts/ssh_chart.png", dpi=250)
--- src/scp-speed-test.sh
+++ src/scp-speed-test.sh
@@ -20,33 +20,33 @@ fi
 
 
 # generate a file of all zeros
-echo "Generating $test_size MB test file..."
+# echo "Generating $test_size MB test file..."
 dd if=/dev/zero of=$test_file bs=$(echo "$test_size*1024*1024" | bc) \
   count=1 &> /dev/null
 # upload test
-echo "Testing upload to $ssh_server..."
+# echo "Testing upload to $ssh_server..."
 up_speed=$(scp -v $test_file $ssh_server:$test_file 2>&1 | \
   grep "Bytes per second" | \
   sed "s/^[^0-9]*\([0-9.]*\)[^0-9]*\([0-9.]*\).*$/\1/g")
   up_speed=$(echo "($up_speed/1000)" | bc)
 
 # download test
-echo "Testing download from $ssh_server..."
+# echo "Testing download from $ssh_server..."
 down_speed=$(scp -v $ssh_server:$test_file $test_file 2>&1 | \
   grep "Bytes per second" | \
   sed "s/^[^0-9]*\([0-9.]*\)[^0-9]*\([0-9.]*\).*$/\2/g")
 down_speed=$(echo "($down_speed/1000)" | bc)
 
 # clean up
-echo "Removing test file on $ssh_server..."
+# echo "Removing test file on $ssh_server..."
 ssh $ssh_server "rm $test_file"
-echo "Removing test file locally..."
+# echo "Removing test file locally..."
 rm $test_file
 
 
 # print result
-echo ""
+# echo "Upload_speed,Download_speed" >> ssh_speed_test.csv
 # echo "Upload speed:   $up_speed KBps"
 # echo "Download speed: $down_speed KBps"
 
-echo "$up_speed,$down_speed\n"
+echo "$up_speed,$down_speed" 
