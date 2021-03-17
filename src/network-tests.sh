9a932dee2df6f6eeb9e5862232eb77d54390dd2e
pmessan <p.messan@jacobs-university.de>
2021-03-17 13:32:34 +0100

    fixed argument bug

--- src/network-tests.sh
+++ src/network-tests.sh
@@ -23,7 +23,7 @@ Usage: ./network-test-script remote_host local_host connection_method trial_loca
 
 removeLastLine() {
   length=$(wc -c < "$1")
-  if [ "$length" -ne 0 ] && [ -z "$(tail -c -1 <file)" ]; then
+  if [ "$length" -ne 0 ] && [ -z "$(tail -c -1 <"$1")" ]; then
     # The file ends with a newline or null
     ( dd if=/dev/null of="$1" obs="$((length-1))" seek=1 ) > /dev/null 2>&1
   fi
